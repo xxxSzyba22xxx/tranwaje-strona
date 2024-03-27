3d7187db3e5af9575ea19bd24e41651960134a38
xxxSzyba22xxx <152599556+xxxSzyba22xxx@users.noreply.github.com>
2024-03-27 09:35:51 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,15 @@
 function hideCookies() {
 var button1 = document.getElementById("confirmButton");
+
+
 var button2 = document.getElementById("declineButton");
+
+var blockdown1 = document.getElementById("cookied");
+var blockdown2 = document.getElementByClassaName("");
+
+button1.addEventListener('click', () => {
+   
+    blockdown1.style.display = 'none'; 
+  });
 
 }
