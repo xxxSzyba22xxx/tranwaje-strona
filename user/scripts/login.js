cdbf710772f44e045cdea2783e243b9e8595f4a5
xxxSzyba22xxx <152599556+xxxSzyba22xxx@users.noreply.github.com>
2024-03-29 12:28:13 +0100

    Update login.js

--- user/scripts/login.js
+++ user/scripts/login.js
@@ -1,1 +1,18 @@
-script
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById("submit");
+    button.addEventListener('click', login);
+});
+
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        alert("Wprowadź login i hasło!");
+    } else if (username === "owner" && password === "stasioowner") {
+        console.log("Owner logged in!");
+        window.location.href = "https://tranwaje-strona.pages.dev/user/owner/user";
+    } else {
+        alert("Nieprawidłowy login lub hasło. Spróbuj ponownie.");
+    }
+}
